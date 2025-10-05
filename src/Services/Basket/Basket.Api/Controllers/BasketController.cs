7c5f2c4cdeed32dd50b183bc9ece87d53fd687ab
mojtaba zare <mojtaba.zare8131@gmail.com>
2025-10-05 04:25:48 -0700

    basket addet

---
+++ src/Services/Basket/Basket.Api/Controllers/BasketController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Basket.Api.Controllers
+{
+    [Route("api/basket")]
+    [ApiController]
+    public class BasketController : ControllerBase
+    {
+        [HttpGet("get-all")]
+        public async Task<IActionResult> GetAllBasket()
+        {
+            return StatusCode(200, "get all");
+        }
+    }
+}
